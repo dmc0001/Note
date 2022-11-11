4352f3ba1852c15215030995a832cead5607c430
lorenzo medici <lorenzomedici2001@gmail.com>
2022-11-11 22:38:51 +0100

    Note-App

--- app/src/main/java/com/example/note/PreInterFaceActivity.java
+++ app/src/main/java/com/example/note/PreInterFaceActivity.java
@@ -1,14 +1,33 @@
 package com.example.note;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class PreInterFaceActivity extends AppCompatActivity {
+    CardView cardViewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_inter_face);
+        cardViewNote = findViewById(R.id.card_view_note);
+
+        cardViewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PreInterFaceActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(PreInterFaceActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
